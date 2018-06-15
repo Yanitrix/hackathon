33bb435bad2a9721cba9df85b0b03039a9a541c2
Daniel Tomala <danieltomala@gazeta.pl>
2018-06-15 22:15:25 +0200

    Navigate Drawer updates

--- app/src/main/java/com/example/btb/schoolbattle/MainActivity.java
+++ app/src/main/java/com/example/btb/schoolbattle/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.btb.schoolbattle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -71,19 +72,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_ranking) {
 
-        } else if (id == R.id.nav_slideshow) {
+            startActivity(new Intent(MainActivity.this,LaderboardActivity.class));
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_esport) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sport) {
 
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
