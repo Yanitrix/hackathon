f93b4b741acddfe29a30afebdc857eebe7ca8fd3
Daniel Tomala <danieltomala@gazeta.pl>
2018-06-16 03:05:04 +0200

    Laderboard doesn't work

--- app/src/main/java/com/example/btb/schoolbattle/LaderboardActivity.java
+++ app/src/main/java/com/example/btb/schoolbattle/LaderboardActivity.java
@@ -1,42 +1,34 @@
 package com.example.btb.schoolbattle;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class LaderboardActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private ListView leaderboard;
-    private Spinner periodChooser;
 
     CustomAdapter weeklyAdapter;
     CustomAdapter monthlyAdapter;
     CustomAdapter yearlyAdapter;
-
     String[] CLANS_WEEKLY = {"Slowack", "Sniadek", "Sawcia"};
     String[] CLANS_MONTHLY = {"Slowack", "Sniadek", "Sawcia"};
     String[] CLANS_YEARLY = {"Slowack", "Sniadek", "Sawcia"};
-
     int[] POINTS_YEARLY = {1260, 540, 2};
     int[] POINTS_MONTHLY = {520, 234, 34};
     int[] POINTS_WEEKLY = {234, 123, 42};
-
-    int[] IMGS = {R.drawable.logo_icon};
-
+    int[] IMGS = {R.drawable.logo_icon, R.drawable.logo_icon, R.drawable.logo_icon};
+    private ListView leaderboard;
+    private Spinner periodChooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +42,9 @@ public class LaderboardActivity extends AppCompatActivity implements AdapterView
         periodChooser.setAdapter(ar);
         periodChooser.setOnItemSelectedListener(this);
 
-         weeklyAdapter = new CustomAdapter(this, CLANS_WEEKLY, POINTS_WEEKLY, IMGS);
-         monthlyAdapter = new CustomAdapter(this, CLANS_MONTHLY, POINTS_MONTHLY, IMGS);
-         yearlyAdapter = new CustomAdapter(this, CLANS_YEARLY, POINTS_YEARLY, IMGS);
+//         weeklyAdapter = new CustomAdapter(this, CLANS_WEEKLY, POINTS_WEEKLY, IMGS);
+        monthlyAdapter = new CustomAdapter(this, CLANS_MONTHLY, POINTS_MONTHLY, IMGS);
+        yearlyAdapter = new CustomAdapter(this, CLANS_YEARLY, POINTS_YEARLY, IMGS);
 
         leaderboard.setAdapter(weeklyAdapter);
 
@@ -64,15 +56,15 @@ public class LaderboardActivity extends AppCompatActivity implements AdapterView
 
     }
 
-    private void displayWeekly(){
+    private void displayWeekly() {
         leaderboard.setAdapter(weeklyAdapter);
     }
 
-    private void displayMonthly(){
+    private void displayMonthly() {
         leaderboard.setAdapter(monthlyAdapter);
     }
 
-    private void displayYearly(){
+    private void displayYearly() {
         leaderboard.setAdapter(yearlyAdapter);
     }
 
@@ -103,19 +95,18 @@ public class LaderboardActivity extends AppCompatActivity implements AdapterView
     }
 
 
-    class CustomAdapter extends ArrayAdapter<String>
-    {
+    class CustomAdapter extends ArrayAdapter<String> {
         Context context;
         String clanNames[];
         int scores[];
         int[] imgs;
 
-        CustomAdapter(Context c, String[] names, int[] scores, int[] imgs){
+        CustomAdapter(Context c, String[] names, int[] scores, int[] imgs) {
             super(c, R.layout.listview_layout, R.id.textView_clanname, names);
             this.context = c;
-            this.imgs =  imgs;
+            this.imgs = imgs;
             this.clanNames = names;
-            this.scores =  scores;
+            this.scores = scores;
 
         }
 
@@ -123,9 +114,9 @@ public class LaderboardActivity extends AppCompatActivity implements AdapterView
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater lfi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View view  = lfi.inflate(R.layout.listview_layout, parent, false);
+            View view = lfi.inflate(R.layout.listview_layout, parent, false);
             ImageView imgs = view.findViewById(R.id.clanLogo);
-            TextView clanName =  view.findViewById(R.id.textView_clanname);
+            TextView clanName = view.findViewById(R.id.textView_clanname);
             TextView score = view.findViewById(R.id.textView_score);
             imgs.setImageResource(position);
             clanName.setText(clanNames[position]);
