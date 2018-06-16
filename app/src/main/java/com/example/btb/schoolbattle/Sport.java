4fa509c78eddd6dae28594c963c829ac3dbe8513
Daniel Tomala <danieltomala@gazeta.pl>
2018-06-16 10:56:40 +0200

    AddingActivity Work

--- app/src/main/java/com/example/btb/schoolbattle/Sport.java
+++ app/src/main/java/com/example/btb/schoolbattle/Sport.java
@@ -1,6 +1,8 @@
 package com.example.btb.schoolbattle;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,14 +15,14 @@ public class Sport extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sport);
 
-        Button button = (Button) findViewById(R.id.floatingActionButton);
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.floatingActionButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Sport.this,AddingActivity.class));
+               startActivity(new Intent(Sport.this,AddingActivity.class));
             }
         });
 
-    }
+   }
 }
